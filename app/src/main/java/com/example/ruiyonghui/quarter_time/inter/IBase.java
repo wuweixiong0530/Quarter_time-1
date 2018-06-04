79ec661c4ef62614668148e28d2f2049e25e9ef4
ruiyonghui <love6958@qq.com>
2018-06-04 17:14:49 +0800

    阿萨斯

--- app/src/main/java/com/example/ruiyonghui/quarter_time/inter/IBase.java
+++ app/src/main/java/com/example/ruiyonghui/quarter_time/inter/IBase.java
@@ -2,5 +2,6 @@ package com.example.ruiyonghui.quarter_time.inter;
 
 public interface IBase {
     int getContenLayout();
+
     void inject();
 }
--- app/src/main/java/com/example/ruiyonghui/quarter_time/ui/MainActivity.java
+++ app/src/main/java/com/example/ruiyonghui/quarter_time/ui/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.ruiyonghui.quarter_time.ui;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,9 +9,32 @@ import com.example.ruiyonghui.quarter_time.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    Handler handler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_main );
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+
+                try {
+                    sleep(2000);
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Intent intent=new Intent(MainActivity.this,HomeActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    });
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
     }
 }
