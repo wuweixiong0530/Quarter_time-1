404dcadd2f8e532f0bcc8b4bf2c3023abe24239a
fanhonest <1736314202@qq.com>
2018-06-04 21:31:17 +0800

    Bottom TabBar

--- app/src/main/java/com/example/ruiyonghui/quarter_time/fragment/VideoFragment.java
+++ app/src/main/java/com/example/ruiyonghui/quarter_time/fragment/VideoFragment.java
@@ -7,9 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.example.ruiyonghui.quarter_time.R;
-
 public class VideoFragment extends Fragment {
     @Nullable
     @Override
