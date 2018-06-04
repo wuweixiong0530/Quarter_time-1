adca0dbabaee0dc5d03db2e7cea48f0aa817c4d1
fanhonest <1736314202@qq.com>
2018-06-04 21:30:43 +0800

    Bottom TabBar

--- app/src/main/java/com/example/ruiyonghui/quarter_time/fragment/RecommendFragment.java
+++ app/src/main/java/com/example/ruiyonghui/quarter_time/fragment/RecommendFragment.java
@@ -8,13 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.ruiyonghui.quarter_time.R;
+
 public class RecommendFragment  extends Fragment{
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-
-
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.recommend, container, false);
+        return view;
     }
 }
--- app/src/main/java/com/example/ruiyonghui/quarter_time/fragment/VideoFragment.java
+++ app/src/main/java/com/example/ruiyonghui/quarter_time/fragment/VideoFragment.java
@@ -1,4 +1,21 @@
 package com.example.ruiyonghui.quarter_time.fragment;
 
-public class VideoFragment {
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.ruiyonghui.quarter_time.R;
+
+public class VideoFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.videofragment, container, false);
+        return view;
+    }
 }
