fc4706bca65cbca66d8fe5c4e26d3d08b05472a3
fanhonest <1736314202@qq.com>
2018-06-04 21:11:27 +0800

    Bottom TabBar

--- app/src/main/java/com/example/ruiyonghui/quarter_time/fragment/RecommendFragment.java
+++ app/src/main/java/com/example/ruiyonghui/quarter_time/fragment/RecommendFragment.java
@@ -1,4 +1,20 @@
 package com.example.ruiyonghui.quarter_time.fragment;
 
-public class RecommendFragment {
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RecommendFragment  extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
