98b351587dcf54e7a3506cf34ff8f5d9554f0f44
ruiyonghui <love6958@qq.com>
2018-06-14 15:12:52 +0800

    登录+第三方登录+注册oK了+底部切换颜色+我的关注+段子+夜间模式

--- app/src/main/java/com/example/ruiyonghui/quarter_time/ui/HomeActivity.java
+++ app/src/main/java/com/example/ruiyonghui/quarter_time/ui/HomeActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.Window;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -65,7 +66,7 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         }
 //        可以设置主题的 方法 在oncreate之前调用
         setTheme(theme);
-
+        supportRequestWindowFeature( Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_home);
 
         intiView();
